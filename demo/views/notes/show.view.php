46ef41a0921c9592511840b68e57e63b7627df11
Aly-Mohsen <aly.mohsen1000@gmail.com>
2025-08-21 14:23:47 +0300

    Add note editing functionality
    Introduced controllers and views to support editing notes, including form validation and update logic. Updated routes and note display to provide edit links and improved UI for navigation and actions.

--- demo/views/notes/show.view.php
+++ demo/views/notes/show.view.php
@@ -5,16 +5,28 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <p class="mb-6">
-            <a href="/notes" class="text-blue-500 underline">go back...</a>
+            <a href="/notes" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Back</a>
         </p>
 
         <p><?= htmlspecialchars($note['body']) ?></p>
 
-        <form class="mt-6" method="POST">
-            <input type="hidden" name = "_method" value = "DELETE">
-            <input type="hidden" name="id" value="<?= $note['id'] ?> ">
-            <button class="text-sm text-red-500">Delete</button>
-        </form>
+        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6 flex gap-x-4 mt-6 justify-center">
+            <a 
+                href="/note/edit?id=<?= $note['id']?>" 
+                class="mt-3 mb-3 inline-flex justify-center rounded-md border border-transparent bg-yellow-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+                Edit
+            </a>
+
+            <form method="POST">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="id" value="<?= $note['id'] ?> ">
+                <button
+                    class="mt-3 mb-3 inline-flex justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Delete</button>
+            </form>
+
+        </div>
+
     </div>
 </main>
 
