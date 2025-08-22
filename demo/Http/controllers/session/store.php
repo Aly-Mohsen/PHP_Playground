d7ea9758107d6beb1f21d97b2a69a7472b9e5655
Aly-Mohsen <aly.mohsen1000@gmail.com>
2025-08-22 21:48:17 +0300

    Refactor controllers structure and improve login form
    Moved controllers to the Http/controllers directory and updated route definitions to match the new structure. Added LoginForm class for login validation and refactored session/store.php to use it. Updated Router to resolve controller paths accordingly.

---
+++ demo/Http/controllers/session/store.php
@@ -0,0 +1,41 @@
+<?php
+
+use Core\App;
+use Core\Validator;
+use Core\Database;
+use Http\Forms\LoginForm;
+
+$db = App::resolve(Database::class);
+
+$email    = $_POST["email"];
+$password = $_POST["password"];
+
+$form = new LoginForm();
+
+if (!$form->validate($email, $password)) {
+    return view('session/create.view.php', [
+        'errors' => $form->errors()
+    ]);
+}
+
+$user = $db->query('select * from users where email = :email', [
+    'email' => $email
+])->find();
+
+if ($user) {
+    if (password_verify($password, $user['password'])) {
+        login([
+            'email' => $email
+        ]);
+        header('location: /');
+        exit();
+    }
+
+}
+
+return view('session/create.view.php', [
+    'errors' => [
+        'email' => 'No matching account for the provided email address or password.'
+    ]
+]);
+
